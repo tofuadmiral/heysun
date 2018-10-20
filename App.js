ec165dd2c7dea98302565d6cafb8c41d122556df
unknown <b_rian_chiu@hotmail.com>
2018-10-20 07:54:15 -0400

    splash page added

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Images } from 'react-native';
-import Splash from './components/splash'
-import Login from './components/login'
+import { AppRegistry , StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends Component {
+//------------------------------splash
+class Splash extends Component {
   render() {
     return (
-      <Splash />
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require('./assets/bed.png')}/>
+          <Text style={styles.title}>HeySun</Text>
+        </View>
+      </View>
+    );
+  }
+
+}
+
+//-----------------------------------login
+
+class Login extends Component{
+  render() {
+    return(
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require('./assets/bed.png')}/>
+          <Text style={styles.header1}>Welcome to HeySun</Text>
+        </View>
+        <View style={styles.formContainer}>
+
+        </View>
+
+      </View>
+    );
+  }
+
+}
+
+//---------------------------------loginForm
+
+class LoginForm extends Component{
+  render() {
+    return(
+      <View>
+
+      </View>    
     );
   }
 }
 
+//---------------------------------------app
+
+export default class App extends Component {
+  render() {
+    return (
+     <Login />
+    );
+  }
+}
+
+
+
+//------------------------------------------style sheet
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#9059ff',
   },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 60,
+  },
+  header1: {
+    color: '#fff',
+    fontSize: 24
+  },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 120,
+    height: 120
+  },
+
 });
+
+AppRegistry.registerComponent('heysun', () => App);
--- README.md
+++
@@ -1,2 +0,0 @@
-# heysun
-social alarm clock app designed to reward those who wake up with a head start
